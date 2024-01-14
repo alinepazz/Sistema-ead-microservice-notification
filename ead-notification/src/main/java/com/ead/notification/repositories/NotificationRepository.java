6939b9b0559ac2c4625e86c19a3aca0e79dbb770
Aline Paz <aline.soares.2@hotmail.com>
2024-01-14 14:38:12 -0300

    correção nome de metodo

--- ead-notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
+++ ead-notification/src/main/java/com/ead/notification/repositories/NotificationRepository.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
-    Page<NotificationModel> findAllByUserIdAndNotificationSatus(UUID userId, NotificationSatus notificationSatus, Pageable pageable);
+    Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationSatus notificationSatus, Pageable pageable);
 
 
     Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId);
